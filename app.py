be278a19360121cf16ab1e7be7855fb1282239e7
aanshi gahlot <aanshigahlot01@gmail.com>
2025-07-31 00:14:58 +0530

    this is my final commit

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import pickle
-from flask import Flask, request, app, jsonify, render_template,url_for
+from flask import Flask, request, jsonify, app, render_template, url_for
 import numpy as np
 import pandas as pd
 
 app = Flask(__name__)
 # Load the pre-trained model
-regmodel= pickle.load(open('regmodel.pkl', 'rb'))
-scalar= pickle.load(open('scaling.pkl', 'rb'))
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scaler = pickle.load(open('scaling.pkl', 'rb'))
+
 
 @app.route('/')
 def home():
@@ -14,20 +15,22 @@ def home():
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data=request.json['data']
+    data = request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data= scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1)) #standardization
+    output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input= scaler.transform(np.array(data).reshape(1,-1))
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
-    return render_template('home.html', prediction_text='The House Price prediction is ${}'.format(regmodel.predict(final_input)[0])) 
+    regmodel.predict(final_input)
+    output = regmodel.predict(final_input)[0]
+    return render_template('home.html', prediction_text='The predicted house price is ${}'.format(output))
 
-if __name__ == "__main__":
+if __name__== "__main__":
     app.run(debug=True)
